bf4ce0e579980b7757866924a85e6b2d280764cf
Victor Ursan <victor.ursan@gmail.com>
2015-10-14 12:16:42 +0300

    Initial Commit

---
+++ testVPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testVPN
+//
+//  Created by Victor Ursan on 10/14/15.
+//  Copyright © 2015 Victor Ursan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
